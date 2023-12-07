e3571620ca0f54037e5cfeb038555de3a3c92674
Dhanesh <dadheech.dhanesh50@gmail.com>
2023-12-08 00:00:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const API_KEY = "0209293bf2b04c8eab4c2bae72c783ae";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load",() => fetchNews("india"));
+
+async function fetchNews(query){
+    const res= await fetch(`${url}${query}&apikey=${API_KEY}`);
+    const data = await res.json();
+    bindData(data.articles);
+}
+
+function bindData(articles){
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+    cardsContainer.innerHTML = "";
+
+    articles.forEach(article => {
+        if(!article.urlToImage) return;
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone,article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+function fillDataInCard(cardClone,article){
+    const newsImg = cardClone.querySelector('#news-img');
+    const newsSource = cardClone.querySelector('#news-source');
+    const newsTitle = cardClone.querySelector('#news-title');
+    const newsDesc = cardClone.querySelector('#news-desc');
+
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML= article.title;
+    newsDesc.innerHTML=  article.description;
+    
+    const date = new Date(article.publishedAt).toLocaleString("en-Us",{
+        timeZone: "Asia/Jakarta"
+    });
+     newsSource.innerHTML = `${article.source.name} ${date}`;
+
+     cardClone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"blank");
+     })
+    }
+
+     function onNavItemClick(id){
+        fetchNews(id);
+     }
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click",() => {
+    const query = searchText.value;
+    if(!query)return;
+    fetchNews(query);
+});
+function reload(){
+    window.location.reload();
+}
